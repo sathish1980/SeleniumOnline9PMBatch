24b0bb15fc9f5a76376c596a08f58dd72f668099
sathish1980 <kumar.sathish189@gmail.com>
2023-06-27 21:43:06 +0530

    New file added

---
+++ src/main/java/testSelenium/SeleniumOnline9PMBatch/TestGit.java
@@ -0,0 +1,14 @@
+package testSelenium.SeleniumOnline9PMBatch;
+
+public class TestGit {
+
+	public void testData()
+	{
+		System.out.println("TEst");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
